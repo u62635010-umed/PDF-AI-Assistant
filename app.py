c556b9ef80cbc22ad3f8bf0b05e3a3701eeb2db6
Umed bhati <u62635010@gmail.com>
2026-02-09 22:11:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from dotenv import load_dotenv
-from PyPDF2 import PdfReader
+from pypdf import PdfReader
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.embeddings import OpenAIEmbeddings
 from langchain.vectorstores import FAISS
@@ -106,3 +106,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
